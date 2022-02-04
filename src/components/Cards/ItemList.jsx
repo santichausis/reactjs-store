a421a50fa696cba44d7c4f9aff714cc48eb4eaa3
Santiago Chausis <u523982@teco.com.ar>
2022-02-04 16:23:16 -0300

    Nuevos componentes

---
+++ src/components/Cards/ItemList.jsx
@@ -0,0 +1,26 @@
+import { Outlet } from "react-router-dom";
+import { CardContainer, Card, Image, StyledLink, CardTitle, CardBrand, CardPrice, CardCategory } from './Style'
+import { data } from '../../utils/Data'
+
+const Cards = () => {
+
+    return (
+        <CardContainer>
+            {data.map(({ id, imagen, nombre, marca, precio, categoria, stock }) =>
+                <Card key={id}>
+                    <StyledLink
+                        to={`/item/${id}`}
+                        >
+                        <Image src={`../assets/boards/${imagen}`} alt={nombre} />
+                        <CardTitle>{nombre}</CardTitle>
+                        <CardBrand>{marca}</CardBrand>
+                        <CardPrice>u$s {precio}</CardPrice>
+                        <CardCategory>Tabla: {categoria}</CardCategory>
+                    </StyledLink>
+                </Card>
+            )}
+            <Outlet />
+        </CardContainer>
+    )
+}
+export default Cards;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
+import Cards from '../components/Cards/ItemList';
+import NotFound from './404'
+import ItemDetail from "../components/Cards/ItemDetail";
+import Cart from "./Cart";
+
+const Home = () => {
+  return (
+    <div className='App'>
+      <Header />
+        <Routes>
+          <Route path='/' element={<Cards />} />
+            <Route path='/item/:idItem' element={<ItemDetail />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path='*' element={<NotFound />}></Route>
+        </Routes>
+      <Footer/>
+    </div>
+  );
+}
+
+export default Home;
