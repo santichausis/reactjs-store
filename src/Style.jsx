237fb0fe302e87e5e3b4dddee3b4a57168ac5f48
Santiago Chausis <u523982@teco.com.ar>
2022-02-11 12:58:57 -0300

    Se ajustan estilos de botones

--- src/Style.jsx
+++ src/Style.jsx
@@ -4,13 +4,32 @@ const CartContainer = styled.section`
     width: 1140px;
     margin: 0 auto;
 `
+const DeleteProduct = styled.button`
+     margin: 10px;
+    padding: 5px 10px;
+    text-align: center;
+    font-size: 14px;
+    text-decoration: none;
+    color: #3b2974;
+    background: transparent;
+    cursor: pointer;
+    transition: ease-out 0.5s;
+    border-radius: 30px;
+    border: 2px solid #725AC1;
+    border-radius: 10px;
+    box-shadow: inset 0 0 0 0 #725AC1;
+    :hover {
+    color: white;
+    box-shadow: inset 0 -100px 0 0 #725AC1;
+`
 const CartProduct = styled.div`
     display: grid;
     grid-template-columns: 300px 1fr;
+    padding-top: 20px;
 `
 const CartImage = styled.img`
-    width: 200px;
+    width: 240px;
     border-radius: 10px;
 `
 
-export { CartContainer, CartProduct, CartImage }
+export { CartContainer, DeleteProduct, CartProduct, CartImage }
