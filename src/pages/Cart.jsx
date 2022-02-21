45082228401dcc3ab86bbbb91246d2be4485bc75
Santiago Chausis <u523982@teco.com.ar>
2022-02-21 11:17:41 -0300

    Se manda parte del ticket

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,9 +2,44 @@ import { useContext } from 'react';
 import { CartContext } from '../context/CartContext';
 import { CartContainer, CartImage, CartProduct, DeleteProduct } from '../Style';
 import { Link } from 'react-router-dom';
+import { collection, doc, setDoc, serverTimestamp, updateDoc, increment } from 'firebase/firestore';
+import { db } from '../utils/FirebaseConfig';
 
 const Cart = () => {
     const cart = useContext(CartContext);
+
+    const createOrder = () => {
+        const itemsForDB = cart.cartList.map(item => ({
+            idItem: item.id,
+            nombre: item.nameItem,
+            precio: item.precio
+        }));
+
+        cart.cartList.forEach(async (item) => {
+            const itemRef = doc(db, 'data', item.idItem);
+            await updateDoc(itemRef, {
+                stock: increment(-item.cantidadItem)
+            });
+        });
+
+        let order = {
+            total: cart.calcTotal(),
+            items: itemsForDB,
+            date: serverTimestamp()
+        };
+
+        const createOrderInFirestore = async () => {
+            // Add a new document with a generated id
+            const newOrderRef = doc(collection(db, 'orders'));
+            await setDoc(newOrderRef, order);
+            return newOrderRef;
+        }
+
+        createOrderInFirestore()
+            .then(result => alert('Orden procesada correctamente 🎫. Número de orden: ' + result.id + '\n\n'))
+        cart.removeList();
+
+    }
 
     return (
         <CartContainer>
@@ -28,6 +63,23 @@ const Cart = () => {
                         </div>
                     </CartProduct>
                 ))}
+            </>
+            <>
+                {
+                    cart.cartList.length > 0 &&
+                    <div>
+                        <h3>Resumen de compra</h3>
+                        <div>
+                            <p>Subtotal</p>
+                            <p>{cart.calcSubTotal()}</p>
+                        </div>
+                        <div type='total'>
+                            <p>Total</p>
+                            <p>{cart.calcTotal()}</p>
+                        </div>
+                        <buttom onClick={createOrder}>ORDENAR 💳</buttom>
+                    </div>
+                }
             </>
         </CartContainer>
     );
